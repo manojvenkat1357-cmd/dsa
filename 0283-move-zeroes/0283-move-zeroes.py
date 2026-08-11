a4837e24ef69c1add8fa519828eaab0b01690bd5
manojvenkat1357-cmd <manojvenkat1357@gmail.com>
2026-08-11 14:05:23 +0530

    Time: 7 ms (55.85%), Space: 13.6 MB (49.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+       i=0
+       for j in range (len(nums)):
+        if nums[j]!=0:
+            nums[j],nums[i]= nums[i],nums[j]
+            i+=1
+        
